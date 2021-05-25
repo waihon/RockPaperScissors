9bf0c1d9aac3ba3d8d11d8975a6e9f8eae24fef9
Waihon Yew <yewwaihon@gmail.com>
2021-05-26 01:11:09 +0800

    Initial version of UI

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -10,16 +10,28 @@ import SwiftUI
 struct ContentView: View {
   static let rock = "Rock"
   static let paper = "Paper"
-  static let scissors = "Scissors"  
+  static let scissors = "Scissors"
   static let moves = [rock, paper, scissors]
+  static let winners = [rock: paper, paper: scissors, scissors: rock]
+  static let losers = [rock: scissors, paper: rock, scissors: paper]
   
-  @State private var currentChoice = Int.random(in: 0 ..< moves.count)
+  @State private var appMove = Int.random(in: 0 ..< moves.count)
   @State private var shouldWin = Bool.random()
   @State private var score = 0
+  @State private var playerMove = 0
   
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    VStack {
+      Text("Player's Score: \(score)")
+      Text("App's Move: \(ContentView.moves[appMove])")
+      Text("Player Should \(shouldWin ? "Win" : "Lose")")
+      Text("Player's Move:")
+      ForEach(0 ..< ContentView.moves.count) { number in
+        Button(ContentView.moves[number]) {
+        }
+      }
+      Spacer()
+    }
   }
 }
 
