31672e0ed9841ab6663624b5976adb24cdc1b68b
Waihon Yew <yewwaihon@gmail.com>
2021-05-26 00:22:38 +0800

    Declare class and instance properties

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  static let rock = "Rock"
+  static let paper = "Paper"
+  static let scissors = "Scissors"  
+  static let moves = [rock, paper, scissors]
+  
+  @State private var currentChoice = Int.random(in: 0 ..< moves.count)
+  @State private var shouldWin = Bool.random()
+  @State private var score = 0
+  
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
